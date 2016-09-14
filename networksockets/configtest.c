247e1bef568426c36811e7519ae3e36dbc497d95
Dylan Herman <dah242@scarletmail.rutgers.edu>
2016-09-14 22:35:54 +0000

    configs.c

---
+++ networksockets/configtest.c
@@ -0,0 +1,21 @@
+#include "configs.h"
+
+
+
+
+
+
+
+int 
+main( int argc, char *argv[]){
+
+char *file = "`tc.conf";
+#define NUM_SERVS;
+struct service *servs = (struct service *servs)malloc(
+
+
+
+exit(EXIT_SUCCESS);
+
+}
+
